07c01e8fa448e10040e2e0fd8a5b87e3e56e6c2d
daria <pilyasova.d@gmail.com>
2019-07-10 21:19:27 +0300

    hometask_2

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,13 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(len: Int = 16): String {
+    val result = this.trim()
+    return if (result.length <= len + 1) result else "${result.take(len + 1).trimEnd()}..."
+}
+
+fun String.stripHtml(): String {
+    return this
+        .replace("""<.*?>""".toRegex(), "")
+        .replace("""&(#\d+?|\w+?);""".toRegex(), "")
+        .split(""" +""".toRegex()).joinToString(" ")
+}
